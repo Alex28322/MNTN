d16b2493131c90674654001e87c5112ff145d110
Alex28322 <sander2832@gmail.com>
2025-04-23 00:16:32 +0300

    first draft

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const target = document.querySelector(".hero")
+target.addEventListener('mousemove', e => {
+Object.assign(document.documentElement, {
+    style:`
+    --move-x: ${(e.clientX - window.innerWidth / 2)* -0.001}deg;
+    --move-y: ${(e.clientY - window.innerHeight / 2)* -0.001}deg;
+    `
+    })
+}
+)
