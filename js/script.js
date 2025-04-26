24b087f98aa8b835e87b1f1be2504e78557857dc
Alex28322 <sander2832@gmail.com>
2025-04-26 23:20:22 +0300

    second_var

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const target = document.querySelector(".hero")
 target.addEventListener('mousemove', e => {
 Object.assign(document.documentElement, {
     style:`
-    --move-x: ${(e.clientX - window.innerWidth / 2)* -0.001}deg;
-    --move-y: ${(e.clientY - window.innerHeight / 2)* -0.001}deg;
+    --move-x: ${(e.clientX - window.innerWidth / 2)* 0.001}deg;
+    --move-y: ${(e.clientY - window.innerHeight / 2)* 0.001}deg;
     `
     })
 }
